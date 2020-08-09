be855319040fb145ed780a72b0cffe6030e1b432
Robin Stenvi <robin.stenvi@gmail.com>
2020-08-09 20:01:05 +0200

    Moved network configuration to library

--- src/include/aspos_lwip.h
+++ src/include/aspos_lwip.h
@@ -3,8 +3,29 @@
 
 #include "lwip/api.h"
 #include "lwip/sys.h"
+#include "lwip/sockets.h"
 
 #define ETH_DRIVER "/ethernet"
+
+
+/*
+* lwip/sockets.h creates macros for some of the function names we use in our
+* regular syscalls. If any of these are needed, the function name prefixed with
+* "lwip_" should be used instead.
+*/
+#undef read
+#undef close
+#undef fcntl
+#undef ioctl
+#undef write
+
+
+/* is in
+* #include "netif/ethernet.h"
+* But that is not included in image
+*/
+err_t ethernet_input(struct pbuf *p, struct netif *netif);
+
 
 struct aspos_ethif {
 	int fd;
@@ -16,5 +37,10 @@ void tcp_thread(void* arg);
 err_t aspos_netif_init(struct netif* netif);
 void aspos_netif_read_loop(void* arg);
 
+
+
+// ------------------------ network.c ------------------------------ //
+#define NET_INIT_DEFAULT_ROUTE (1 << 0)
+int network_init_dhcp(long flags);
 
 #endif
---
+++ src/userlib/network.c
@@ -0,0 +1,52 @@
+/**
+* Reasonable configurations of network.
+*/
+
+#include "lib.h"
+#include "lwip/api.h"
+#include "lwip/dhcp.h"
+#include "aspos_lwip.h"
+
+#define MAX_WAIT_TICKS 30
+
+int network_init_dhcp(long flags)	{
+	struct netif* n = (struct netif*)malloc( sizeof(struct netif) );
+	int count, ret;
+
+	/* Start network interface without having an IP attached */
+	if(netif_add_noaddr(n, NULL, aspos_netif_init, ethernet_input) == NULL)	{
+		printf("netif_add returned NULL\n");
+		return -(GENERAL_FAULT);
+	}
+
+	/* Check if interface is up */
+	if(!netif_is_up(n))	{
+		printf("Interface is not up\n");
+		return -(GENERAL_FAULT);
+	}
+
+	ret = dhcp_start(n);
+	if(ret != ERR_OK)	{
+		printf("dhcp_start() returned %i\n", ret);
+		return -(GENERAL_FAULT);
+	}
+	
+	// Check if we have gotten an IP 
+	count = 0;
+	do {
+		tsleep(2);
+		count += 2;
+	} while((!dhcp_supplied_address(n)) && (count < MAX_WAIT_TICKS));
+
+	if(count > MAX_WAIT_TICKS)	{
+		printf("No IP from DHCP\n");
+		return -(GENERAL_FAULT);
+	}
+
+	if(FLAG_SET(flags, NET_INIT_DEFAULT_ROUTE))	{
+		netif_set_default(n);
+	}
+
+	return OK;
+}
+
