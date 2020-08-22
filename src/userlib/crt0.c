26895f9899ac0544b101feb0ddb701e9b16789d1
Robin Stenvi <robin.stenvi@gmail.com>
2020-08-22 07:58:49 +0200

    Support seed of random numbers in crt0

--- src/userlib/crt0.c
+++ src/userlib/crt0.c
@@ -1,9 +1,14 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
  
 extern void _exit(int);
 extern int main ();
- 
+
+#if CONFIG_INIT_RANDOM_SEED
+static void _seed_random(void);
+#endif
+
 void _start(uint64_t argc, uint64_t argv, uint64_t envp) {
 	/*
 	* Open stdin, stdout and stderr
@@ -19,7 +24,28 @@ void _start(uint64_t argc, uint64_t argv, uint64_t envp) {
 	err = dup(out);
 	if(err != 2)	_exit(err);
 
+#if CONFIG_INIT_RANDOM_SEED
+	_seed_random();
+#endif
+
     int ex = main(argc, argv, envp);
     _exit(ex);
 }
 
+#if CONFIG_INIT_RANDOM_SEED
+static void _seed_random(void)	{
+	int res;
+	unsigned int seed;
+	int rand = open("/dev/random", 0, 0);
+	if(rand < 0)	{
+		puts("Failed to open random driver\n");
+		_exit(rand);
+	}
+	res = read(rand, &seed, sizeof(unsigned int));
+	if(res != sizeof(unsigned int))	{
+		puts("Failed to read acquired number of bytes from random driver\n");
+		_exit(res);
+	}
+	srandom(seed);
+}
+#endif
